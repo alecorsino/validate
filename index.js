8deb4ec21c1a928d576e36885264064784cd05af
Alejandro Corsino <alecorsino@gmail.com>
2018-12-03 22:56:11 -0800

    Initial setup
    Functional data validation

---
+++ index.js
@@ -0,0 +1,29 @@
+import { compose } from 'ramda';
+
+const gt0 = (acc) => {
+  if (acc.value < 1) {
+    acc = validationError(acc, 'Not greater than zero');
+  }
+  return acc
+}
+const isNumber = (acc) => {
+  if (typeof acc.value !== 'number') {
+    acc = validationError(acc, 'Not a Number')
+  }
+  
+  return acc
+}
+
+const validationError = (acc, err_str) =>{
+  const errors = [...acc.errors, err_str];
+  return {
+   errors,
+   pass : false,
+    value:acc.value,
+}
+}
+const initValue = (val) => ({value:val, pass:true, errors:[]})
+
+const isNumberGreaterThanZero = compose( isNumber, gt0,);
+
+isNumberGreaterThanZero(initValue(0)); 
